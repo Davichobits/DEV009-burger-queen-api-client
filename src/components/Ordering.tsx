a7d087aa3c426eda21a848c7e1c36e60a26e21ca
Davichobits <davidruizdeveloper@gmail.com>
2023-09-22 17:24:05 -0500

    Buttons to select product types

--- src/components/Ordering.tsx
+++ src/components/Ordering.tsx
@@ -6,6 +6,10 @@ export const Ordering = () => {
   const [products, setProducts] = useState([])
   const [token, ]  = useLocalStorage('token')
 
+  const allProductsTypes = products.map((product: interfaceProduct) => product.type)
+  const productsTypes = [...new Set(allProductsTypes)]
+  
+
   useEffect(() => {
     const options = {
       method: "GET",
@@ -27,10 +31,19 @@ export const Ordering = () => {
     image: string;
     price: number;
   }
-  console.log(products);
   return (
     <>
     <h1 className="text-center text-2xl font-bold my-4">Tomar pedido</h1>
+    <div className="flex justify-center gap-16">
+      {
+        productsTypes.map((type: string) => (
+          <button key={type} className="border border-gray-200 max-w-[500px] rounded-lg mb-4 p-3 cursor-pointer hover:bg-gray-200">
+            {type}
+          </button>
+        ))
+      }  
+    </div>
+    
     {
       products.map((product: interfaceProduct) => (
         <div key={product.id} className="border border-gray-200 max-w-[500px] rounded-lg mb-4 mx-auto p-3 flex justify-between items-center cursor-pointer hover:bg-gray-200">
